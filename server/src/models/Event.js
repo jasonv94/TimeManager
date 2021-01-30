5cdf6421f9309cc969d76a5047d10ef6a199533a
William Wright <william.wright3@carleton.ca>
2021-01-30 16:30:34 -0500

    Models

--- server/src/models/Event.js
+++ server/src/models/Event.js
@@ -1,8 +1,35 @@
 const mongoose = require("mongoose")
 
-const schema = mongoose.Schema({
-    title: String,
-    content: String,
+const eventSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    allDay: {
+        type: boolean,
+        required: true
+    },
+    start: {
+        type: String,
+        required: true
+    },
+    end: {
+        type: String
+    },
+    url: {
+        type: String,
+        trim: true
+    },
+    backgroundColor: {
+        type: String
+    },
+    borderColor: {
+        type: String
+    },
+    textColor: {
+        type: String
+    }
 })
-
+        
 module.exports = mongoose.model("Event", schema)
