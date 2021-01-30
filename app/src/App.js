b114a6d63536eff3752c0e751beb1db8c9cddedb
fbarinas <barins.flavio@gmail.com>
2021-01-30 13:17:25 -0500

    chore: setup react layout

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import { Overview } from './pages';
+
 import './App.css';
 
+
 function App() {
+  const routes = (
+    <Switch>
+      <Route
+        exact={true}
+        path='/'
+        component={Overview}
+      />
+    </Switch>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Router>
+        <React.Fragment>
+          {routes}
+        </React.Fragment>
+      </Router>
+    </React.Fragment>
   );
 }
 
