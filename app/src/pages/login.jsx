09938c9e305313156331635ddb7ccb831218b23f
fbarinas <barins.flavio@gmail.com>
2021-01-30 19:26:12 -0500

    fix

--- app/src/pages/login.jsx
+++ app/src/pages/login.jsx
@@ -1,58 +1,98 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import TextField from "@material-ui/core/TextField";
 
-// import axios from "axios";
+import axios from "axios";
 
-// import { Banner } from "../components";
+class Login extends React.Component {
 
-async function loginUser(credentials) {
-    // return fetch('http://localhost:8080/login', {
-    //     method: 'POST',
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     },
-    //     body: JSON.stringify(credentials)
-    // })
-    //     .then(data => data.json())
-    return { "token": credentials }
-}
-
-function Login({ setToken }) {
-    const [username, setUserName] = useState();
-    const [password, setPassword] = useState();
-
-    const handleSubmit = async e => {
-        e.preventDefault();
-        const token = await loginUser({
-            username,
-            password
-        });
-        setToken(token);
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            password: "",
+        };
+        this.set_username = this.set_username.bind(this);
+        this.set_password = this.set_password.bind(this);
+        this.handle_submit = this.handle_submit.bind(this);
+        this.handle_register = this.handle_register.bind(this);
     }
 
-    return (
-        <div className="container">
-            <form onSubmit={handleSubmit}>
-                <label>
-                    <p>Username</p>
-                    <TextField onChange={e => setUserName(e.target.value)} />
-                </label>
-                <label>
-                    <p>Password</p>
-                    <TextField
-                        label="Password"
-                        type="password"
-                        onChange={e => setPassword(e.target.value)}
-                    />
-                </label>
-                <div>
-                    <button type="submit">Submit</button>
-                </div>
-            </form>
-        </div>
-    )
+    render() {
+        const { username, password } = this.state;
+        return (
+            <div className="container">
+                <form>
+                    <label>
+                        <p>Username</p>
+                        <TextField
+                            inputProps={{ "value": username }}
+                            onChange={e => this.set_username(e.target.value)} />
+                    </label>
+                    <label>
+                        <p>Password</p>
+                        <TextField
+                            inputProps={password}
+                            label="Password"
+                            type="password"
+                            onChange={e => this.set_password(e.target.value)}
+                        />
+                    </label>
+                    <div>
+                        <button type="submit" onClick={this.handle_submit}>Login</button>
+                        <button type="submit" onClick={this.handle_register}>Register</button>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+
+    set_username(username) {
+        this.setState({ username });
+    }
+
+    set_password(password) {
+        this.setState({ password });
+    }
+
+    handle_submit(e) {
+        e.preventDefault();
+        const { username, password } = this.state;
+        console.log("logging in: " + username);
+
+        const config = {
+            headers: { 'Access-Control-Allow-Origin': '*' }
+        };
+        axios
+            .post('http://localhost:5000/api/login', { username, password }, config)
+            .then(res => {
+                this.props.setToken(res.data);
+                console.log("successful");
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    handle_register(e) {
+        e.preventDefault();
+        const { username, password } = this.state;
+        console.log("register: " + username);
+
+        const config = {
+            headers: { 'Access-Control-Allow-Origin': '*' }
+        };
+        axios
+            .post('http://localhost:5000/api/register', { username, password }, config)
+            .then(res => {
+                this.props.setToken(res.data);
+                console.log("successful");
+            })
+            .catch(err => {
+                console.log("err: " + err)
+            })
+    }
 }
 
 Login.propTypes = {
