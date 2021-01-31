ec907bf9c989d40222854417c1af74d6ad6ef332
jasonv94 <varghe0@unbc.ca>
2021-01-30 19:04:52 -0800

    added ui

--- app/src/pages/overview.jsx
+++ app/src/pages/overview.jsx
@@ -6,6 +6,7 @@ class Overview extends React.Component {
     render() {
         return (
             <div className="container">
+               
                 <Banner />
                 Overview
             </div>
